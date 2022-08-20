271fc1fcc44f626101bf3e207f870ce05f541e67
Milon <jan.milon.56@gmail.com>
2022-08-20 23:54:49 +0600

    case Price increase and decrase done

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-// document.getElementById('case-plus').addEventListener('click', function(){
-//     const quantity = document.getElementById('case-qnt');
-//     const quantityString = quantity.value;
-//     const previousQuantity = parseInt(quantityString);
-//     const newQuantity = previousQuantity +1;
-//     quantity.value = newQuantity;
-// });
-// document.getElementById('case-minus').addEventListener('click', function(){
-//     const quantity = document.getElementById('case-qnt');
-//     const quantityString = quantity.value;
-//     const previousQuantity = parseInt(quantityString);
-//     const newQuantity = previousQuantity - 1;
-//     quantity.value = newQuantity;
-// });
-function updateCaseNumber(isIncrease, id){
+
+function updateCaseNumber(isIncrease, id) {
 
     const quantity = document.getElementById(id);
     const quantityString = quantity.value;
     const previousQuantity = parseInt(quantityString);
     let newQuantity;
-    if(isIncrease === true){
+    if (isIncrease === true) {
         newQuantity = previousQuantity + 1;
-    }else{
+    } else {
         newQuantity = previousQuantity - 1;
     }
     quantity.value = newQuantity;
-}
-document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber(true, 'case-qnt');
-})
-document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber(false, 'case-qnt');
-})
+    return newQuantity;
+};
+document.getElementById('case-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true, 'case-qnt');
+    const totalCasePrice = newCaseNumber * 59;
+    const casePrice = document.getElementById('case-price');
+    casePrice.innerText = totalCasePrice;
+
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(false, 'case-qnt');
+    const totalCasePrice = newCaseNumber * 59;
+    const casePrice = document.getElementById('case-price');
+    casePrice.innerText = totalCasePrice;
+});
